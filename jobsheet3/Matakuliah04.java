a866efed33f22534d9bdd764da6410274911151c
arielardani <arielardani20@gmail.com>
2026-02-24 15:52:24 +0700

    Modifikasi Percibaan 3.4

--- jobsheet3/Matakuliah04.java
+++ jobsheet3/Matakuliah04.java
@@ -1,5 +1,7 @@
 package jobsheet3;
 
+import java.util.Scanner;
+
 public class Matakuliah04 {
    
     public String kode;
@@ -7,6 +9,7 @@ public class Matakuliah04 {
     public int sks;
     public int jumlahJam;
     
+    static Scanner sc = new Scanner(System.in);
     public Matakuliah04(){
 
     }
@@ -15,6 +18,27 @@ public class Matakuliah04 {
         this.nama = nama;
         this.sks = sks;
         this.jumlahJam = jumlahJam;
+    }
+    public void tambahData() {
+        String dummy;
+        System.out.print("Kode       : ");
+        this.kode = sc.nextLine();
+        System.out.print("Nama       : ");
+        this.nama = sc.nextLine();
+        System.out.print("Sks        : ");
+        dummy = sc.nextLine();
+        this.sks = Integer.parseInt(dummy);
+        System.out.print("Jumlah Jam : ");
+        dummy = sc.nextLine();
+        this.jumlahJam = Integer.parseInt(dummy);
+        System.out.println("--------------------------------------");
+    }
+    void cetakInfo(){
+         System.out.println("Kode       : " + kode);
+            System.out.println("Nama       : " + nama);
+            System.out.println("Sks        : " + sks);
+            System.out.println("Jumlah Jam : " + jumlahJam);
+            System.out.println("--------------------------------------");
     }
 }
 
