43acdd54886db5aaa090b466e977e33fb0e4083e
arielardani <arielardani20@gmail.com>
2026-04-21 08:21:47 +0700

    Percobaan1

---
+++ jobsheet9/Mahasiswa04.java
@@ -0,0 +1,23 @@
+package jobsheet9;
+
+public class Mahasiswa04 {
+    String nama;
+    String nim;
+    String kelas;
+    int nilai;
+
+    Mahasiswa04(String nama, String nim, String kelas) {
+        this.nama = nama;
+        this.nim = nim;
+        this.kelas = kelas;
+        nilai = -1;
+    }
+
+    void tugasDinilai(int nilai) {
+        this.nilai = nilai;
+    }
+
+    Mahasiswa04[] stack;
+    int top;
+    int size;
+}
