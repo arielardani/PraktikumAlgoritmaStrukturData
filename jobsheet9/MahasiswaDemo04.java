bb4fb934c11197c3ada027f2dab9629859e0a634
arielardani <arielardani20@gmail.com>
2026-04-21 08:58:35 +0700

    Pertanyaan 2.1.3

--- jobsheet9/MahasiswaDemo04.java
+++ jobsheet9/MahasiswaDemo04.java
@@ -1,10 +1,13 @@
 package jobsheet9;
 
+import java.net.Socket;
+import java.nio.channels.Pipe.SourceChannel;
 import java.util.Scanner;
 
 public class MahasiswaDemo04 {
     public static void main(String[] args) {
         int pilih;
+        int totalKumpul=0;
         Scanner scan = new Scanner(System.in);
         StackTugasMahasiswa04 stack = new StackTugasMahasiswa04(5);
         do {
@@ -12,7 +15,9 @@ public class MahasiswaDemo04 {
             System.out.println("1. Mengumpulkan Tugas");
             System.out.println("2. Menilai Tugas");
             System.out.println("3. Melihat Tugas Teratas");
-            System.out.println("4. Melihat Daftar Tugas");
+            System.out.println("4. Melihat Tugas Terbawah");
+            System.out.println("5. Melihat Daftar Tugas");
+            System.out.println("6. Tugas yang sudah dikumpulkan saat ini");
             System.out.print("Pilih: ");
             pilih = scan.nextInt();
             scan.nextLine();
@@ -27,6 +32,7 @@ public class MahasiswaDemo04 {
                     Mahasiswa04 mhs = new Mahasiswa04(nama, nim, kelas);
                     stack.push(mhs);
                     System.out.printf("Tugas %s berhasil dikumpulkan\n", mhs.nama);
+                    totalKumpul++;
                     break;
                 case 2:
                     Mahasiswa04 dinilai = stack.pop();
@@ -45,13 +51,37 @@ public class MahasiswaDemo04 {
                     }
                     break;
                 case 4:
+                    Mahasiswa04 lihat2 = stack.peekbwh();
+                    if (lihat2 != null) {
+                        System.out.println("Tugas pertama dikumpulkan oleh " + lihat2.nama);
+                    }
+                    break;
+                case 5:
                     System.out.println("Daftar semua tugas");
                     System.out.println("Nama\tNIM\tKelas");
                     stack.print();
                     break;
+                case 6:
+                    System.out.println("1. Termasuk yang sudah dinilai");
+                    System.out.println("2. Tidak Termasuk yang sudah dinilai");
+                    System.out.print("pilih: ");
+                    int opsi =scan.nextInt();
+                    switch (opsi) {
+                        case 1:
+                    System.out.println("Total semua tugas Termasuk yang sudah dinilai adalah " + totalKumpul);  
+                            break;
+                        case 2:
+                    System.out.println("Total semua tugas Tidak Termasuk yang sudah dinilai "+ (stack.top+1) );
+                            break;
+                    
+                        default:
+                            System.out.println("Pilihan tidak valid.");
+                            break;
+                    }
+                    break;
                 default:
                     System.out.println("Pilihan tidak valid.");
             }
-        } while (pilih >= 1 && pilih <= 4);
+        } while (pilih >= 1 && pilih <= 6);
     }
 }
--- jobsheet9/StackTugasMahasiswa04.java
+++ jobsheet9/StackTugasMahasiswa04.java
@@ -55,9 +55,17 @@ public class StackTugasMahasiswa04 {
             return null;
         }
     }
+    public Mahasiswa04 peekbwh() {
+        if (!isEmpty()) {
+            return stack[0];
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas yang dikumpulkan");
+            return null;
+        }
+    }
 
     public void print() {
-        for (int i = 0; i <= top; i++) {
+        for (int i = top; i >= 0; i--) {
             System.out.println(stack[i].nama + "\t" + stack[i].nim + "\t" + stack[i].kelas);
         }
         System.out.println("");
