0a0efa9e7922ecd1fcb62aa6ba8d39bcdb655b28
arielardani <arielardani20@gmail.com>
2026-06-02 10:39:34 +0700

    perbaikan typo folder name

---
+++ Pertemuan14/Node04.java
@@ -0,0 +1,10 @@
+package Pertemuan14;
+public class Node04 {
+    Mahasiswa04 mahasiswa;
+    Node04 left;
+    Node04 right;
+    Node04 (Mahasiswa04 mahasiswa){
+        this.mahasiswa = mahasiswa;
+        left = right = null;
+    }
+}
